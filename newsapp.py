b7dbf956c9cd81831655637e4ff2ff24a0df3ca0
Archit Kar <88377838+Archit-24-Kar@users.noreply.github.com>
2022-08-04 00:44:01 +0530

    Add files via upload

---
+++ newsapp.py
@@ -0,0 +1,27 @@
+from cProfile import label
+import requests
+import tkinter as tk
+def getNews():
+    api_key="ea8bedbeffc647a184fa49e89d2ecc4f"
+    url="https://newsapi.org/v2/top-headlines?country=in&apiKey="+api_key
+    news=requests.get(url).json()
+    articles=news["articles"]
+    my_articles=[]
+    my_news=""
+    for article in articles:
+        my_articles.append(article["title"])
+    for i in range(10):
+        my_news=my_news+str(i+1)+"."+ my_articles[i]+"\n "
+    label.config(text=my_news)
+canvas =tk.Tk()
+canvas.geometry("900x600")
+canvas.title("News App")
+
+
+button=tk.Button ( canvas,font=24,text="Reload",command=getNews)
+button.pack(pady=20)
+
+label=tk.Label(canvas,font=18,justify="left")
+label.pack(pady=20)
+getNews()
+canvas.mainloop()
